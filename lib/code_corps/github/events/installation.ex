56983cb324cb853612235caedc18a7798475582d
Nikola Begedin <begedinnikola@gmail.com>
2017-06-28 11:08:30 +0200

    Implement event handling flow up to repo creation

--- lib/code_corps/github/events/installation.ex
+++ lib/code_corps/github/events/installation.ex
@@ -10,7 +10,7 @@ defmodule CodeCorps.GitHub.Events.Installation do
     User
   }
 
-  import Ecto.Query, only: [where: 3]
+  alias Ecto.Changeset
 
   @doc """
   Handles an "Installation" GitHub Webhook event
@@ -20,29 +20,71 @@ defmodule CodeCorps.GitHub.Events.Installation do
   - do the work
   - marked the passed in event as "processed" or "errored"
   """
-  def handle(%GithubEvent{action: "created"}, %{"installation" => %{"id" => installation_id}, "sender" => %{"id" => user_id}}) do
-    # check if a user with the GitHub account in the `sender` exists
-    case user_exists?(user_id) do
-      %User{} = user -> IO.inspect(user) # match installation
-      nil -> IO.inspect("no user") # create installation
-    end
-
-    # if no such user exists, then create an installation with
-    # `unmatched_user`
-
-    # if a user exists, but no matching GithubAppInstallation, then
-    # create one with `initiated_on_github`
-
-    # if the user and GitHubAppInstallation match, then update the
-    # installation to processing and fetch all the repos from the API
-    # to display on the project integrations page
+  def handle(%GithubEvent{action: "created"} = event, payload) do
+    event
+    |> start_event_processing()
+    |> do_handle(payload)
+    |> stop_event_processing(event)
   end
 
-  def handle(%GithubEvent{}, _payload), do: :not_fully_implemented
+  defp start_event_processing(%GithubEvent{} = event) do
+    event |> Changeset.change(%{status: "processing"}) |> Repo.update()
+  end
 
-  defp user_exists?(github_id) do
-    User
-    |> where([u], u.github_id == ^github_id)
-    |> Repo.one
+  defp do_handle({:ok, %GithubEvent{}}, %{"installation" => installation_attrs, "sender" => sender_attrs}) do
+    case {sender_attrs |> find_user, installation_attrs |> find_installation} do
+      {nil, nil} -> create_unmatched_user_installation(installation_attrs)
+      {%User{} = user, nil} -> create_installation_initiated_on_github(user, installation_attrs)
+      {%User{} = user, %GithubAppInstallation{} = github_app_installation} -> update_installation(user, github_app_installation)
+    end
+  end
+
+  defp stop_event_processing({:ok, %GithubAppInstallation{}}, %GithubEvent{} = event) do
+    event |> Changeset.change(%{status: "processed"}) |> Repo.update
+  end
+
+  defp stop_event_processing(_, %GithubEvent{} = event) do
+    event |> Changeset.change(%{status: "errored"}) |> Repo.update
+  end
+
+  defp find_user(%{"id" => github_id}), do: User |> Repo.get_by(github_id: github_id)
+
+  defp find_installation(%{"id" => github_id}), do: GithubAppInstallation |> Repo.get_by(github_id: github_id)
+
+  defp create_unmatched_user_installation(%{"id" => github_id}) do
+    %GithubAppInstallation{}
+    |> Changeset.change(%{state: "unmatched_user", github_id: github_id})
+    |> Repo.insert()
+  end
+
+  defp create_installation_initiated_on_github(%User{} = user, %{"id" => github_id}) do
+    %GithubAppInstallation{}
+    |> Changeset.change(%{state: "initiated_on_github", github_id: github_id})
+    |> Changeset.put_assoc(:user, user)
+    |> Repo.insert()
+  end
+
+  defp update_installation(%User{} = user, %GithubAppInstallation{} = github_app_installation) do
+    github_app_installation
+    |> start_app_processing()
+    |> process_repos()
+    |> stop_app_processing()
+  end
+
+  defp start_app_processing(%GithubAppInstallation{} = github_app_installation) do
+    github_app_installation
+    |> Changeset.change(%{state: "processing"})
+    |> Repo.update()
+  end
+
+  defp process_repos({:ok, %GithubAppInstallation{} = github_app_installation}) do
+    # TODO: Implement this part
+    {:ok, github_app_installation}
+  end
+
+  defp stop_app_processing({:ok, %GithubAppInstallation{} = github_app_installation}) do
+    github_app_installation
+    |> Changeset.change(%{state: "processed"})
+    |> Repo.update()
   end
 end
--- test/lib/code_corps/github/events/installation_test.exs
+++ test/lib/code_corps/github/events/installation_test.exs
@@ -7,16 +7,66 @@ defmodule CodeCorps.GitHub.Events.InstallationTest do
   import CodeCorps.TestHelpers.GitHub
 
   alias CodeCorps.{
+    GithubAppInstallation,
     GithubEvent,
-    GitHub.Events.Installation
+    GitHub.Events.Installation,
+    Repo
   }
 
   describe "handle/2" do
-    test "is not implemented" do
+    test "creates installation for unmatched user if no user" do
       payload = load_fixture("installation_created")
-      %{"sender" => %{"id" => user_id}} = payload
-      insert(:user, github_id: user_id)
-      assert Installation.handle(%GithubEvent{action: "created"}, payload) == :not_fully_implemented
+      event = insert(:github_event, action: "created", type: "installation")
+
+      assert Installation.handle(event, payload)
+
+      github_app_installation = Repo.one(GithubAppInstallation)
+      assert github_app_installation.github_id == (payload |> get_in(["installation", "id"]))
+      assert github_app_installation.state == "unmatched_user"
+      refute github_app_installation.user_id
+
+      updated_event = Repo.one(GithubEvent)
+      assert updated_event.status == "processed"
+    end
+
+    test "creates installation initiated_on_github if user matched but installation unmatched" do
+      %{"sender" => %{"id" => user_github_id}} = payload = load_fixture("installation_created")
+      event = insert(:github_event, action: "created", type: "installation")
+
+      user = insert(:user, github_id: user_github_id)
+
+      assert Installation.handle(event, payload)
+
+      github_app_installation = Repo.one(GithubAppInstallation)
+      assert github_app_installation.github_id == (payload |> get_in(["installation", "id"]))
+      assert github_app_installation.state == "initiated_on_github"
+      assert github_app_installation.user_id == user.id
+
+      updated_event = Repo.one(GithubEvent)
+      assert updated_event.status == "processed"
+    end
+
+    test "updates installation, creates repos, if both user and installation matched" do
+      %{"sender" => %{"id" => user_github_id}, "installation" => %{"id" => installation_github_id}} = payload = load_fixture("installation_created")
+      event = insert(:github_event, action: "created", type: "installation")
+
+      user = insert(:user, github_id: user_github_id)
+      github_app_installation = insert(:github_app_installation, github_id: installation_github_id, user: user)
+
+      assert Installation.handle(event, payload)
+
+      updated_github_app_installation = Repo.one(GithubAppInstallation)
+      assert updated_github_app_installation.github_id == (payload |> get_in(["installation", "id"]))
+      assert updated_github_app_installation.state == "processed"
+      assert updated_github_app_installation.user_id == user.id
+      assert updated_github_app_installation.github_id == installation_github_id
+      assert updated_github_app_installation.id == github_app_installation.id
+
+      updated_event = Repo.one(GithubEvent)
+      assert updated_event.status == "processed"
+
+      # TODO: Get repos from github, insert into DB
+      assert false
     end
   end
 end
